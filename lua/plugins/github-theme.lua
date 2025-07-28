8607b0617222e2c968b4a3f688ff78a781b13a56
Mark Holmes <mark.j.holmes.95@gmail.com>
2025-07-28 11:20:42 +0200

    theme change, todo lines, comments plugin

--- lua/plugins/github-theme.lua
+++ lua/plugins/github-theme.lua
@@ -3,10 +3,10 @@ return {
   "projekt0n/github-nvim-theme",
   name = "github-theme",
   -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "github_dark_default",
-    },
-  },
+  --  {
+  --    "LazyVim/LazyVim",
+  --    opts = {
+  --      colorscheme = "github_dark_default",
+  --    },
+  --  },
 }
---
+++ lua/plugins/monokai-pro.lua
@@ -0,0 +1,10 @@
+return {
+  "loctvl842/monokai-pro.nvim",
+  -- Configure LazyVim to load
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai-pro",
+    },
+  },
+}
