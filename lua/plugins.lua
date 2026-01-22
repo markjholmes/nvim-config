c2d47949b58edb205fbce930538740faf9107328
Mark Holmes <markjamestheholmes@gmail.com>
2026-01-22 21:10:53 +0100

    removed relative line nubmers, added bracket pairing

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -298,6 +298,15 @@ require("lazy").setup({
     {
         "quarto-dev/quarto-nvim",
     },
+
+    {
+        'altermo/ultimate-autopair.nvim',
+        event={'InsertEnter','CmdlineEnter'},
+        branch='v0.6', --recommended as each new version will have breaking changes
+        opts={
+            --Config goes here
+        },
+    }
 })
 
 -- INSTALL
