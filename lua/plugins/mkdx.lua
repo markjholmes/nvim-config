a8c1aaf43eb68fb7cfc1c90d2fb345e66343c143
Mark Holmes <markjamestheholmes@gmail.com>
2026-02-01 23:02:42 +0100

    removed everything from lazy vim

--- lua/plugins/mkdx.lua
+++
@@ -1,8 +0,0 @@
--- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
-return {
-  "SidOfc/mkdx",
-  opts = {
-    -- add any options here
-  },
-  config = function() end,
-}
