75b4f9e0ebc0ae1f1a8b4c032920756fd1ef8d80
Mark Holmes <mark.j.holmes.95@gmail.com>
2026-06-12 15:59:05 +0200

    floating diagnostics

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -3,3 +3,23 @@ vim.lsp.enable('clangd')
 vim.lsp.enable('r_language_server')
 vim.lsp.enable('pyright')
 vim.lsp.enable('marksman')
+
+vim.diagnostic.config({
+    virtual_text  = true,
+    severity_sort = true,
+    float         = {
+        style  = 'minimal',
+        border = 'rounded',
+        source = 'if_many',
+        header = '',
+        prefix = '',
+    },
+    signs         = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '✘',
+            [vim.diagnostic.severity.WARN]  = '▲',
+            [vim.diagnostic.severity.HINT]  = '⚑',
+            [vim.diagnostic.severity.INFO]  = '»',
+        },
+    },
+})
